a5b4f1673ebd9f061f3a128d108aa8c8cd7b5092
davidnmqw <xsusli11@gmail.com>
2024-06-07 06:45:14 +0200

    Update codeWars.py
    polish with gpt

--- codeWars.py
+++ codeWars.py
@@ -1,9 +1,13 @@
 t1 = "mnooopqrsttt"
 t2 = "xyzzzzabc"
+# def lngs(t1, t2):
+#     a, t = "", t1 + t2
+#     for i in t:
+#         if i not in a:
+#             a += i
+#     return "".join(sorted(a))
+# print(lngs(t1, t2))
+
 def lngs(t1, t2):
-    a, t = "", t1 + t2
-    for i in t:
-        if i not in a:
-            a += i
-    return "".join(sorted(a))
+    return "".join(sorted(set(t1 + t2)))             #úprava s gpt
 print(lngs(t1, t2))
