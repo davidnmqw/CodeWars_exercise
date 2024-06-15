bdaf289f890dba7e45b7bdd8640580e71b440894
davidnmqw <xsusli11@gmail.com>
2024-06-15 14:14:30 +0200

    Update codeWars.py

--- codeWars.py
+++ codeWars.py
@@ -31,7 +31,43 @@ print(year)
 a = list(range(13,50))
 b = 64
 def two_sum(numbers, target):
-    c = numbers
-    d = target
-    return (c, d)
+    pass
 print(two_sum(a, b))
+
+#break camelCase
+s = "different ThingLittleTry"
+t, z, = "", " "
+for idx, i in enumerate(s):     #klíčem bylo použít funkci enumerate 
+    if i == i.upper() and i != " " and z != " ":
+        t += s[:idx] + " "      #zaítm nefunguje
+        s = s[idx:]
+    z = i
+t += s
+print(" ".join(t.split()))
+
+#další pokus, s využitím původního s beze změny
+s = "different ThingLittleTry"
+t, z, f = "", " ", 0
+for idx, i in enumerate(s):     #klíčem bylo použít funkci enumerate 
+    if i == i.upper() and i != " " and z != " ":
+        t += s[f:idx] + " "      #logiku jsem vymyslel sám
+        f = idx                 #gpt to nezvládl vylepšit
+    z = i
+t += s[f:]
+print(" ".join(t.split()))
+
+#for i in s:
+#    if i == i.upper() and i != " " and s[s.index(i) - 1] != " ":
+#        t += s[:s.index(i)] + " "
+#        s = s[s.index(i):]
+#t += s
+#print(t)
+
+#z = " "
+#for i in s:
+#    if i == i.upper() and i != " " and z != " ":
+#        t += s[:s.index(i)] + " "
+#        s = s[s.index(i):]
+#    z = i
+#t += s
+#print(" ".join(t.split()))
